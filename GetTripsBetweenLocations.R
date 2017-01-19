f01179365e6559a193d0b005804ada4fe36f1684
HChandar <charshain@yahoo.com>
2017-01-18 22:24:15 -0500

    Add files via upload

---
+++ GetTripsBetweenLocations.R
@@ -0,0 +1,22 @@
+#Function to subset the taxi data such that only locations of interest are considered
+#trips: Input data frame
+#startpoint: Longitude and Latitude of where the trips of interest should originate
+#endpoint: Longitude and Latitude of where the trips should end
+#Distance: Permissable distance from the coordinates of interest
+
+
+TripsBetweenPoints<-function(trips,startpoint,endpoint,Distance=50)
+{
+  
+  require(geosphere)
+  require(data.table)
+  require(dplyr)
+    TripsBetweenPoints<- setDT(trips)[ ,StartDistance := distHaversine(matrix(c(StartLon, StartLat), ncol = 2),matrix(c(startpoint), ncol = 2))]
+    TripsBetweenPoints<-setDT(trips)[ ,EndDistance := distHaversine(matrix(c(EndLon, EndLat), ncol = 2),matrix(c(endpoint), ncol = 2))]
+    TripsBtn<-TripsBetweenPoints%>%
+        filter(StartDistance<=Distance,EndDistance<=Distance)
+    return(TripsBtn)
+}
+
+
+
